04695805fa8a2c785258dd67fbe19da771305452
Gleb Pishchalo <gleb4ikq@gmail.com>
2019-10-02 20:23:43 +0300

    releasev1

---
+++ ConsoleApplication1/BooksCatalog.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+   public class BooksCatalog
+    {
+        public BooksCatalog()
+        {
+
+        }
+        //public void LoadData()
+        //{
+        //    string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+        //    SqlConnection myConnection = new SqlConnection(connectString);
+
+        //    myConnection.Open();
+        //    {
+        //        //  SqlCommand command = new SqlCommand("SELECT * FROM Authors INNER JOIn  BAConnect inner join books on BAConnect.Book_id = Books.Book_id ON Authors.Author_ID = BAConnect.Author_ID");
+        //        string query = "SELECT Books.book_id,book_name,YEAR,author_name,Authors.author_id FROM Authors INNER JOIn  BAConnect inner join books on BAConnect.Book_id=Books.Book_id ON Authors.Author_ID = BAConnect.Author_ID";
+        //        SqlCommand command = new SqlCommand(query, myConnection);
+
+        //        SqlDataReader reader = command.ExecuteReader();
+
+        //        if (reader.HasRows) // если есть данные
+        //        {
+                    
+        //            // Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}", reader.GetName(0), reader.GetName(1), reader.GetName(2), reader.GetName(3), reader.GetName(4), reader.GetName(5), reader.GetName(6));
+
+
+
+        //            while (reader.Read()) // построчно считываем данные
+        //            {
+        //                object Book_ID = reader.GetValue(0);
+        //                object Book_name = reader.GetValue(1);
+        //                object Year = reader.GetValue(2);
+        //                object Author_name = reader.GetValue(3);
+        //                object Author_id = reader.GetValue(4);
+        //                ;
+
+        //                Console.WriteLine("{0}\t{1}\t{2}\t{3}", reader[0], reader[1], reader[2], reader[3], reader[4]);
+
+        //            }
+        //        }
+
+
+        //        reader.Close();
+
+        //        myConnection.Close();
+
+        //        Console.ReadKey();
+        //    }
+        //}
+        public void LoadBooks()
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+                string query = "SELECT Distinct * from Books";
+                SqlCommand command = new SqlCommand(query, myConnection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Book_ID = reader.GetValue(0);
+                        object Book_name = reader.GetValue(1);
+                        object Year = reader.GetValue(2);
+                        Console.WriteLine("{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+
+                    }
+                }
+
+
+                reader.Close();
+
+                myConnection.Close();
+
+             //   Console.ReadKey();
+            }
+        }
+        public void LoadAuthors()
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+                string query = "SELECT * from Authors";
+                SqlCommand command = new SqlCommand(query, myConnection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Book_ID = reader.GetValue(0);
+                        object Book_name = reader.GetValue(1);
+                        Console.WriteLine("{0}\t{1}", reader[0], reader[1]);
+
+                    }
+                }
+
+
+                reader.Close();
+
+                myConnection.Close();
+
+//                Console.ReadKey();
+            }
+        }
+        public int DeleteBook(int b)
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+                
+                    string query = "Delete from Books where Books.book_id=" + b + "";
+                    SqlCommand command = new SqlCommand(query, myConnection);
+                    myConnection.Close();
+
+                Console.ReadKey();
+            }
+            return b;
+        }
+
+        public int FindBooks(int a)
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+                string query = "SELECT Books.book_id,book_name,YEAR FROM Authors INNER JOIn  BAConnect inner join books on BAConnect.Book_id=Books.Book_id ON Authors.Author_ID = BAConnect.Author_ID where Authors.author_id =" + a + "";
+                SqlCommand command = new SqlCommand(query, myConnection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Author_ID = reader.GetValue(0);
+                        object Author_name = reader.GetValue(1);
+
+                    }
+                }
+
+
+                reader.Close();
+
+                myConnection.Close();
+
+     //           Console.ReadKey();
+            }
+            return a;
+
+        }
+        public int InsertBook(string b,int c)
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+
+                string query = "insert into Books(book_name,year) values ('" + b + "'," + c + ")";
+               //     + "insert into Authors(author_name) values ('" + d + "')"
+              //      + "insert into BAConnect(book_id,author_id) values(SELECT TOP 1 book_id FROM Books ORDER BY book_id DESC,SELECT TOP 1 author_id FROM Authors ORDER BY book_id DESC)";
+                SqlCommand command = new SqlCommand(query, myConnection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Author_ID = reader.GetValue(0);
+                        object Author_name = reader.GetValue(1);
+                        Console.WriteLine("{0}\t{1}", reader[0], reader[1]);
+
+                    }
+                }
+                reader.Close();
+                myConnection.Close();
+     //           Console.ReadKey();
+            }
+            return c;
+        }
+
+        public string InsertAuthors(string d)
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+
+                string query =
+                    "insert into Authors(author_name) values ('" + d + "')";
+                 //   + " insert into BAConnect(book_id,author_id) values(SELECT TOP 1 book_id FROM Books ORDER BY book_id DESC,SELECT TOP 1 author_id FROM Authors ORDER BY book_id DESC)";
+                SqlCommand command = new SqlCommand(query, myConnection);
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Author_ID = reader.GetValue(0);
+                        object Author_name = reader.GetValue(1);
+
+                    }
+                }
+                reader.Close();
+
+                myConnection.Close();
+
+    //            Console.ReadKey();
+            }
+            return d;
+        }
+        public string InsertBA()
+        {
+            string connectString = @"Data Source=(local);Initial Catalog=BooksCatalog;Integrated Security=True";
+
+            SqlConnection myConnection = new SqlConnection(connectString);
+
+            myConnection.Open();
+            {
+     
+                string query ="insert into BAConnect(book_id,author_id) values((SELECT TOP 1 book_id FROM Books ORDER BY book_id DESC),(SELECT TOP 1 author_id FROM Authors ORDER BY author_id DESC))";
+                SqlCommand command = new SqlCommand(query, myConnection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object Author_ID = reader.GetValue(0);
+                        object Author_name = reader.GetValue(1);
+                       // Console.WriteLine("{0}\t{1}", reader[0], reader[1]);
+
+                    }
+                }
+
+
+                reader.Close();
+
+                myConnection.Close();
+
+   //             Console.ReadKey();
+            }
+            return null;
+        }
+    }
+
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+       
+     
+        static void Main(string[] args)
+        {
+            BooksCatalog a = new BooksCatalog();
+           // a.LoadData();
+            Console.WriteLine("Выберите требуемое действие:\n 1.Список книг\n 2. Список авторов\n 3.Добавить книгу\n 4.Удалить книгу\n 5. Книги по коду автора");
+            int b = Convert.ToInt32(Console.ReadLine());
+            switch (b)
+            {
+                
+                case 1: a.LoadBooks(); break;
+                case 2: a.LoadAuthors(); break;
+                case 3:
+                    Console.WriteLine("Введите название книги");
+                    string nazv_knigi;
+                    do { nazv_knigi = Console.ReadLine(); }
+                    while ((nazv_knigi.Length) > 30 && (nazv_knigi.Length <1));
+                    Console.WriteLine("Введите год книги:");
+                    int year_book;
+                    do { year_book= Convert.ToInt32(Console.ReadLine()); }
+                    while ((year_book>2019)&&(year_book<868)); a.InsertBook(nazv_knigi,year_book);
+                    Console.WriteLine("Введите количество авторов:");
+                    int n;
+                    n = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < n; i++)
+                    {
+
+                        Console.WriteLine("Введите автора:");
+                        string auth_knigi;
+                        do { auth_knigi = Console.ReadLine(); }
+                        while ((auth_knigi.Length) > 20 && (auth_knigi.Length < 1));
+                        a.InsertAuthors(auth_knigi);
+                        a.InsertBA();
+                    }
+
+                    break;
+                case 4: Console.WriteLine("Введите код книги:"); try { int del_book = Convert.ToInt32(Console.ReadLine()); a.DeleteBook(del_book); }catch { Console.WriteLine("Neverniy nomer"); }break;
+                case 5: Console.WriteLine("Введите код автора:"); int auth_code = Convert.ToInt32(Console.ReadLine()); a.FindBooks(auth_code);break;
+                default: Console.WriteLine("Некоректные данные");break;
+
+
+            }
+            Console.ReadKey();
+
+           
+        }
+    } 
+}
